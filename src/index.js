51b72647378ce3f5e552e802e14a42f3c0a1a7c0
shanejix <shanejix@vip.163.com>
2020-10-14 21:22:08 +0800

    feat(*): update repos struct

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "./App";
+import Home from "./views/home";
 
 const RENDER_CONTAINER = document.getElementById("app");
 
-render(<App />, RENDER_CONTAINER);
+render(<Home />, RENDER_CONTAINER);
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './app.less'
+import './home.less'
 
 export default () => {
   return (
